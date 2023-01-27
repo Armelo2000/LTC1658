a2097872957fccfaf02b0202a77d41dc632d185a
Armelo2000 <76920586+Armelo2000@users.noreply.github.com>
2023-01-27 14:11:25 +0100

    Update LTC1658.c

--- LTC1658.c
+++ LTC1658.c
@@ -159,3 +159,106 @@ uint16_t LTC1658_ReadData(){
 
 	return result;
 }
+
+/************************************************************************************************
+ * Diese Routine berechnet den Widerstand NTC Thermistor mit dem gegebener Temperatur.
+ * @param: temperatur, das ist temperatur vom NTC Thermistor (in °C)
+ * @return: das ist der Widerstandswert der zu der gegebene Temperatur passt.
+ *
+ * Hinweis
+ *	Temperatur in Kelvin = Temperatur in Celsius + 273,15
+ * Formel
+ *	RT = RN * EXP(B(1/T - 1/TN))
+ *	
+ *	RN – Widerstandswert bei Bezugstemperatur TN (meistens 25°C).
+ *	RT - Widerstandswert bei Temperatur T
+ *	B – Thermistorkonstante (Datenblatt) in Kelvin - Der wert liegt bei ca. 3988 Kelvin
+ ************************************************************************************************/
+float Widerstand_NTC( float temperatur){
+	
+	//Das ist der Widerstandswert von Thermistor bei 25°C der Wert ist 10 KOhm => 10000 Ohm
+	uint16_t RN = 10000;
+
+	//Umrechnung von Celsius zu Kelvin (T25 = 298,15 °K)
+	float T25 = 25 + 273.15;
+	
+	//Die Temperatur wird in Celsius eingegeben. 
+	//Die folgende Zeile macht die Berechnung von Celsius zu Kelvin
+	temperatur = temperatur + 273.15;
+	
+	// x wird in dem exponent verwendet.
+	float x = B_Wert * ( 1/temperatur - 1/T25);
+
+	//Der Widerstandswert bei der gegebene Temperatur wird hier mit zurückgegeben
+	return RN * exp(x);
+
+}
+
+/************************************************************************************************
+ * Diese Routine berechnet anhang den Widerstand von NTC Thermistor die Spannung an dem Thermistor.
+ * @param: RT, Widerstand von NTC Thermistor bei der Temperatur T
+ * @return: die Spannung an den Thermistor.
+ *
+ * Hinweis
+ *	Es wird über ein Spannungsteiler berechnet.
+ 		      _______
+ 	 Uref <______|	 R1  |_______
+		      *******	   |
+		      	10KOhm	   |
+				   |
+				   ******** V
+				   |
+				   |
+				 __|__
+		     Thermistor	 |   | RT
+				 |___| 10KOhm bei 25°C
+				   |
+				   |
+				 
+ * Formel
+ *	UT = RT * (Uref/(R1 + RT))
+ *	
+ *	R1 – Widerstandswert von R1.
+ *	RT - Widerstandswert von Thermistor bei Temperatur T
+ *	Uref – Referenzspannung
+ *	UT - Spannungsabfall am Thermistor
+ ************************************************************************************************/
+float Spannung (float RT){
+	
+	float UT = RT * (Uref/(R1 + RT));
+	return UT;
+}
+
+/************************************************************************************************
+ * Diese Routine berechnet der 14Bit Digitalwert von Spannung anhang der Analog Spannungswert
+ * @param: U, Analog Spannungswert
+ * @return: der 14Bit digital Spannungswert.
+ *
+ * Hinweise
+ *	Es handelt sich um einen 14Bit ADC Berechnung
+ *
+ * Formel 
+ *	Digitalwert = (2^n - 1)/Referenzwert * Analogwert
+ *
+ *	n ist der Anzahl von bit (bei 14Bit, 2^14 - 1 = 16383)
+ *	Referenzwert ist der maximal Analogwert 
+ ************************************************************************************************/
+uint16_t U_digit( float U){
+
+	uint16_t Digit = 16383 * U/Uref;
+	return Digit;
+
+}
+
+/************************************************************************************************
+ * Diese Routine berechnet der 14Bit Digitalwert mit der gegebene Temperatur
+ * @param: temperatur in °C
+ * @return: der 14Bit digitalwert.
+ ************************************************************************************************/
+uint16_t getDigitBy(float temperatur){
+	float R = Widerstand_NTC(temperatur);
+	float U = Spannung(R);
+	uint16_t digit = U_digit(U);
+	return digit;
+}
+
